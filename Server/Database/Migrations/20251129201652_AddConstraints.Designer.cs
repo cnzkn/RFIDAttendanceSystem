be2b78b8da45e249142f80ae388ae4f1861c7c55
cnzkn <245192669+cnzkn@users.noreply.github.com>
2025-11-30 00:48:13 +0200

    Migration for size & range constraints.

--- Server/Database/Migrations/20251129201652_AddConstraints.Designer.cs
+++ Server/Database/Migrations/20251129201652_AddConstraints.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using CloudAPI.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CloudAPI.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20251129201652_AddConstraints")]
+    partial class AddConstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
